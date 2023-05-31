4f3040e9ef8a2f93d54b36e8efdd8a523ac6cb46
nahueldiaz11 <nahuel.kp2010@hotmail.es>
2023-05-31 18:52:14 -0300

    Se agrego para que el usuario pueda ingresa el valor de cada moneda

--- main.js
+++ main.js
@@ -1,35 +1,91 @@
-const selectPesos = document.getElementById("selectPesos");
-const selectDolares = document.getElementById("selectDolares");
-const selctEuros = document.getElementById("selctEuros");
-const selectReales = document.getElementById("selectReales");
+let dolarHoy, euroHoy, realHoy;
 
-const inputPeso = document.getElementById("inputPeso");
-const inputDolar = document.getElementById("inputDolar");
-const inputEuro = document.getElementById("inputEuro");
-const inputReal = document.getElementById("inputReal");
-
-const btnPeso = document.getElementById('btnPeso');
-const btnDolar = document.getElementById('btnDolar');
-const btnEuro = document.getElementById('btnEuro');
-const btnReal = document.getElementById('btnReal');
-
-
-btnPeso.addEventListener('click', calcularValorPeso);
-// btnDolar.addEventListener('click', calcularValorDolar);
-// btnEuro.addEventListener('click', calcularValorEuro);
-// btnReal.addEventListener('click', calcularValorReal);
-
-
+function guardarValores() {
+  dolarHoy = parseFloat(document.getElementById("valorDolar").value);
+  euroHoy = parseFloat(document.getElementById("valorEuro").value);
+  realHoy = parseFloat(document.getElementById("valorReal").value);
+}
 
 function calcularValorPeso() {
-    
-    const selectMoneda = selectPesos.value;
-    const monto = parseFloat(inputPeso.value);
-    if (selectMoneda == "1") {
-        console.log("Ingreso correcto")
-        const montoConvertido = monto * 493;
-        alert(montoConvertido)
+  const selectMoneda = document.getElementById("selectPesos").value;
+  const monto = parseFloat(document.getElementById("inputPeso").value);
+  let montoConvertido;
+
+  switch (selectMoneda) {
+    case "1":
+      montoConvertido = monto / dolarHoy;
+      alert(`Valor convertido: ${montoConvertido}`);
+      break;
+    case "2":
+      montoConvertido = monto / euroHoy;
+      alert(`Valor convertido: ${montoConvertido}`);
+      break;
+    case "3":
+      montoConvertido = monto / realHoy;
+      alert(`Valor convertido: ${montoConvertido}`);
+      break;
+  }
+}
+
+function calcularValorDolar() {
+    const selectMoneda = document.getElementById("selectDolares").value;
+    const monto = parseFloat(document.getElementById("inputDolar").value);
+    let montoConvertidoDolar;
+  
+    switch (selectMoneda) {
+      case "1":
+        montoConvertidoDolar = monto * dolarHoy;
+        alert(`Valor convertido: ${montoConvertidoDolar}`);
+        break;
+      case "2":
+        montoConvertidoDolar = euroHoy / dolarHoy;
+        alert(`Valor convertido: ${montoConvertidoDolar}`);
+        break;
+      case "3":
+        montoConvertidoDolar = monto *  realHoy;
+        alert(`Valor convertido: ${montoConvertidoDolar}`);
+        break;
     }
+  }
 
+function calcularValorEuro() {
+  const selectMoneda = document.getElementById("selectEuros").value;
+  const monto = parseFloat(document.getElementById("inputEuro").value);
+  let montoConvertido;
 
+  switch (selectMoneda) {
+    case "1":
+      montoConvertido = monto * dolarHoy;
+      alert(`Valor convertido: ${montoConvertido}`);
+      break;
+    case "2":
+      montoConvertido = monto * euroHoy;
+      alert(`Valor convertido: ${montoConvertido}`);
+      break;
+    case "3":
+      montoConvertido = monto * realHoy;
+      alert(`Valor convertido: ${montoConvertido}`);
+      break;
+  }
+}
+
+function calcularValorReal() {
+  const selectMoneda = document.getElementById("selectReales").value;
+  const monto = parseFloat(document.getElementById("inputReal").value);
+  let montoConvertido;
+
+  switch (selectMoneda) {
+    case "1":
+      montoConvertido = monto * dolarHoy;
+      alert(`Valor convertido: ${montoConvertido}`);
+      break;
+    case "2":
+      montoConvertido = monto * (dolarHoy/euroHoy);
+      alert(`Valor convertido: ${montoConvertido}`);
+      break;
+    case "3":
+      montoConvertido = monto * (dolarHoy/realHoy);
+      alert(`Valor convertido: ${montoConvertido}`);
+      break;
+  }
 }
